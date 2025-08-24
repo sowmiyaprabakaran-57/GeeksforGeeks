f40afd4cf585b967750c1e3fc32bd46d3de19b5d
Sowmiya Prabakaran <168167244+sowmiyaprabakaran-57@users.noreply.github.com>
2025-08-24 15:39:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Start Coding - Java/start-coding-java.java
@@ -0,0 +1,13 @@
+class Geeks {
+
+    // Function to print hello
+    static void printHello() {
+
+        System.out.println("Hello World");
+        
+    }
+    public static void main(String[] args)
+    {
+        printHello();
+    }
+}
