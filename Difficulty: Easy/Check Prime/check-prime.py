59942a466ab8eec93c9da4d2263614e630ed4fc4
Sowmiya Prabakaran <168167244+sowmiyaprabakaran-57@users.noreply.github.com>
2025-09-03 23:44:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Prime/check-prime.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+n = int(input())
+
+def is_prime(n:int)->bool:
+    if n<=1:
+        return False
+    if n<=3:
+        return True
+    if n%2==0 or n%3==0:
+        return False
+        
+    i=5
+    while i*i<=n:
+        if n%i==0 or n%(i+2)==0:
+            return False
+        i+=6
+    return True
+print(is_prime(n))
